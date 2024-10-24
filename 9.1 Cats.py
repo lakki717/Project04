f0ca8be4ccb36c69ff84fbee6b8223dbec49d97b
Ольга <lakki717@gmail.com>
2024-10-24 22:13:27 +0300

    Создано окно, виджеты и функция для получения изображения с котиком.

--- 9.1 Cats.py
+++ 9.1 Cats.py
@@ -1,1 +1,34 @@
 from tkinter import*
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+from pygame.examples.moveit import load_image
+
+
+def load_image(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Ошибка при загрузке изображения: {e}")
+        return None
+
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+
+url = "https://cataas.com/cat"
+img = load_image(url)
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
